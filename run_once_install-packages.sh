243c75b5d3d12c309334a7ecd54b9d8d058c31da
dequeues <dequeues@outlook.com>
2022-09-13 18:58:36 +0000

    Update a different version of neovim

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 sudo apt-get install exa \
-	neovim \
 	fdfind \
 	tmux
+
+# Neovim 
+wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+sudo mv ./nvim-linux64/bin/nvim /usr/local/bin
+rm nvim-linux64.tar.gz
+rm -rf nvim-linux64
 
 # Neovim plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
