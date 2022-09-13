55f707aedd9da270822bc5ec69d65c62260e58a6
dequeues <dequeues@outlook.com>
2022-09-13 18:53:23 +0000

    Typo

--- run_once_install-shell.sh
+++ run_once_install-shell.sh
@@ -8,4 +8,4 @@ sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install starship prompt
-curl -sS https://starship.rs/install.sh | shs
+curl -sS https://starship.rs/install.sh | sh
