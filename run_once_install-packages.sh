a5a398ea752c4c0fcc20770c150472b06a4113df
dequeues <dequeues@outlook.com>
2023-02-10 06:18:45 +0000

    Update and add if statements

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -4,17 +4,12 @@ sudo apt-get install exa \
 	fdfind \
 	tmux
 
-# Neovim 
-wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
-sudo mv ./nvim-linux64/bin/nvim /usr/local/bin
-rm nvim-linux64.tar.gz
-rm -rf nvim-linux64
-
 # Neovim plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 vim +'PlugInstall --sync' +qa
 
 # asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+if [ ! -d "~/.asdf" ]; then
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+fi
