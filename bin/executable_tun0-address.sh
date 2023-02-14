c31bfad63ad2bd87d227a446347fdf3eaef7cb9f
Nathan <dequeues@users.noreply.github.com>
2023-02-14 13:58:09 +0000

    tun0, not eth0

--- bin/executable_tun0-address.sh
+++ bin/executable_tun0-address.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ip address show eth0 | grep "inet" | head -n 1 | awk '{print $2}' | cut -f1 -d"/"
+ip address show tun0 | grep "inet" | head -n 1 | awk '{print $2}' | cut -f1 -d"/"
